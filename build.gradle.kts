4938b8f0f95a48a942a68d6a8e73e59d104bdd84
donat <donatien.gauthier@ynov.com>
2024-12-10 16:28:18 +0100

    ci/cd + jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.0"
     id("io.spring.dependency-management") version "1.1.6"
+    id("jacoco") version "0.8.7"
 }
 
 group = "com.exercice"
